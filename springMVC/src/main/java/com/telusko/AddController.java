de74b86bf1d54ea88719c3bd2f9194159a6c626a
Santosh <negisantosh@outlook.com>
2020-03-21 01:10:02 +0530

    Created java folder to resolve the issue

--- springMVC/src/main/java/com/telusko/AddController.java
+++ springMVC/src/main/java/com/telusko/AddController.java
@@ -1,9 +1,12 @@
 package com.telusko;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 @Controller
 public class AddController {
 	
-	@RequestMapping("/add")
+	@RequestMapping(value ="/add")
 	public String add() {	
 		System.out.println("I m inside controller now");
 		return "display.jsp";
